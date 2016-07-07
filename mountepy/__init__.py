0ec4d834c15ea5c2ae89cd58dc7716bff0540a4b
Michal Bultrowicz <michalbultrowicz@gmail.com>
2016-07-07 22:58:45 +0200

    refactor(module): Removed reimport of HttpService.

--- mountepy/__init__.py
+++ mountepy/__init__.py
@@ -5,6 +5,6 @@ Spawning and cleaning after given HTTP service processes and Mountebank.
 import pkg_resources
 
 from mountepy.http_service import HttpService, ServiceGroup, wait_for_port
-from mountepy.mountebank import Mountebank, HttpService, HttpStub
+from mountepy.mountebank import Mountebank, HttpStub
 
 __version__ = pkg_resources.get_distribution(__name__).version # pylint: disable=no-member
